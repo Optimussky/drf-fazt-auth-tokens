41eb3bd65c2b6ec8a8d7c3177b939cf774f1545f
Alberto Romero <optimussky05@gmail.com>
2024-06-12 21:26:25 -0600

    First commit

---
+++ server/views.py
@@ -0,0 +1,44 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+
+@api_view(['POST'])
+def login(request):
+    print(request.data)
+
+    # Con get_object_or_404 evitamos tener que validar si existe el Usuario
+    #user = get_object_or_404(User, username=request.data['username'])#username=serializer.data['username']
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(resquest.data['password']):
+        return Response({"Error", "Invalid password"}, status=status.HTTP_400_BAD_REQUEST)
+
+    token, create = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def register(request):
+    print(request.data)
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+        user = User.objects.get(username=serializer.data['username'])
+        user.set_password(serializer.data['password'])
+        user.save()
+
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
+
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
+def profile(request):
+    return Response({})
